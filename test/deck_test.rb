27ae616b1610f74ba3aa607041bb96780cafac13
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-11-26 18:46:48 -0700

    Added deck_test, updated turn_test

---
+++ test/deck_test.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/deck"
+
+class DeckTest < Minitest::Test
+  def test_it_exists
+    deck = Deck.new
+
+    assert_instance_of Deck, deck
+  end
+  
+end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -26,20 +26,20 @@ class TurnTest < Minitest::Test
 
   def test_if_the_guess_is_correct
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-    turn1 = Turn.new("Saturn", card)
-    turn2 = Turn.new("Mercury", card)
+    turn_1 = Turn.new("Saturn", card)
+    turn_2 = Turn.new("Mercury", card)
 
-    refute turn1.correct?
-    assert turn2.correct?
+    refute turn_1.correct?
+    assert turn_2.correct?
   end
 
   def test_it_gives_feedback
     card = Card.new("Which planet is closesst to the sub?", "Mercury", :STEM)
-    turn1 = Turn.new("Saturn", card)
-    turn2 = Turn.new("Mercury", card)
+    turn_1 = Turn.new("Saturn", card)
+    turn_2 = Turn.new("Mercury", card)
 
-    assert_equal "Incorrect", turn1.feedback
-    assert_equal "Correct", turn2.feedback
+    assert_equal "Incorrect", turn_1.feedback
+    assert_equal "Correct", turn_2.feedback
   end
 
 end
