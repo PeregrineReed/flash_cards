afc1b19c529585bfed9265468dbd663c5fe2a5d7
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-11-27 08:39:33 -0700

    Added category test assertions

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -9,7 +9,7 @@ class DeckTest < Minitest::Test
     assert_instance_of Deck, deck
   end
 
-  def test_its_cards_can_be_read
+  def test_cards_can_be_read
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -26,7 +26,7 @@ class DeckTest < Minitest::Test
 
   end
 
-  def test_its_cards_can_be_counted
+  def test_cards_can_be_counted
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -43,6 +43,9 @@ class DeckTest < Minitest::Test
     cards = [card_1, card_2, card_3]
     deck = Deck.new[cards]
 
+    assert_equal 2, deck.cards_in_category(:STEM).count
+    assert_equal 1, deck.cards_in_category(:Geography).count
+    assert_equal 0, deck.cards_in_category("Pop Culture").count
   end
 
 end
