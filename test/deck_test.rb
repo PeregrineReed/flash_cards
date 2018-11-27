98d518cbf932832dc61f609f122ca9097d0ae490
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-11-26 20:35:33 -0700

    Added count test to deck_test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -8,5 +8,28 @@ class DeckTest < Minitest::Test
 
     assert_instance_of Deck, deck
   end
-  
+
+  def test_its_cards_can_be_read
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = ("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    deck.cards.each do |card|
+      assert_instance_of Card, card
+    end
+
+  end
+
+  def test_its_cards_can_be_counted
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = ("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal 3, card.count
+  end
+
 end
