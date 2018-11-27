222d3b07262000236cc35f7c790c655931a0af83
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-11-26 20:51:49 -0700

    Added category test, updated card_3

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -12,12 +12,16 @@ class DeckTest < Minitest::Test
   def test_its_cards_can_be_read
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = ("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
 
     deck.cards.each do |card|
       assert_instance_of Card, card
+    end
+    assert_equal ["Juneau", "Mars", "North north west"],
+    deck.cards.map do |card|
+      card.answer
     end
 
   end
@@ -25,11 +29,20 @@ class DeckTest < Minitest::Test
   def test_its_cards_can_be_counted
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = ("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
 
     assert_equal 3, card.count
+  end
+
+  def test_cards_can_be_seperated_by_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new[cards]
+
   end
 
 end
