f80a56ef7100267dfeb76138b24a2a30ffeef55d
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-11-27 08:53:38 -0700

    Added count and category to deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,8 +3,16 @@ require "./lib/card"
 class Deck
 
   attr_accessor :cards
+  attr_reader :count
   def initialize(cards = [])
     @cards = cards
+    @count = cards.count
+  end
+
+  def cards_in_category(category)
+    @cards.select do |card|
+      card.category == category
+    end
   end
 
 end
