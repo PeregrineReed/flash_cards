e78838b85c6343efd5f6e2daf31ea3b0457dc60c
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-11-27 16:50:14 -0700

    Added test for round's deck

--- test/round_test.rb
+++ test/round_test.rb
@@ -1,9 +1,15 @@
-require "minitest/autorun"
-require "minitest/pride"
-require "./lib/round"
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/round'
 
 class RoundTest < Minitest::Test
 
+  def test_it_starts_with_a_deck
+    deck = Deck.new
+    round = Round.new(deck)
 
-
+    assert_instance_of Round, round
+    assert_instance_of Deck, round.deck
+  end
+  
 end
