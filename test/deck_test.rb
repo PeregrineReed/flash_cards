939f062e707020d3f80d91cf3fde40b7adacb90c
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-11-27 08:59:01 -0700

    Debugged deck_test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -19,11 +19,10 @@ class DeckTest < Minitest::Test
     deck.cards.each do |card|
       assert_instance_of Card, card
     end
-    assert_equal ["Juneau", "Mars", "North north west"],
-    deck.cards.map do |card|
+    answers = deck.cards.map do |card|
       card.answer
     end
-
+    assert_equal ["Juneau", "Mars", "North north west"], answers
   end
 
   def test_cards_can_be_counted
@@ -33,7 +32,7 @@ class DeckTest < Minitest::Test
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
 
-    assert_equal 3, card.count
+    assert_equal 3, cards.count
   end
 
   def test_cards_can_be_seperated_by_category
@@ -41,7 +40,7 @@ class DeckTest < Minitest::Test
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
-    deck = Deck.new[cards]
+    deck = Deck.new(cards)
 
     assert_equal 2, deck.cards_in_category(:STEM).count
     assert_equal 1, deck.cards_in_category(:Geography).count
