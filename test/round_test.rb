b505bf62781cd3bf0a5de587d9caedcfec7e1c6b
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-11-27 17:56:29 -0700

    Added correct guess percentage test

--- test/round_test.rb
+++ test/round_test.rb
@@ -31,7 +31,33 @@ class RoundTest < Minitest::Test
     assert_same card_1, round.current_card
   end
 
+  def test_that_taking_a_turn_creates_a_new_instance_of_the_turn_class
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    new_turn = round.take_turn("Juneau")
+    assert_instance_of Turn, new_turn
+    assert_same round.turns[0], new_turn
+  end
+
+  def test_percentage_of_correct_guesses_are_tracked_in_each_round
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    round.turns[0].correct?
+    assert_equal 100.0, round.percent_correct
+    assert_instance_of Float, round.percent_correct
+  end
+
   
+
 
 
 end
