a0a71f8b77c1900e4a4f43af7112eae5266195f4
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-11-27 21:24:16 -0700

    Added test method for number of correct guesses in a round

--- test/round_test.rb
+++ test/round_test.rb
@@ -56,7 +56,22 @@ class RoundTest < Minitest::Test
     assert_same deck.cards[0], round.current_card && card_2
   end
 
-  def test_percentage_of_correct_guesses_is_logged
+  def test_number_of_correct_guesses
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    assert_equal 1, round.number_correct
+    round.take_turn("Venus")
+    assert_equal 1, round.number_correct
+    round.take_turn("North north west")
+    assert_equal 2, round.number_correct
+  end
+
+  def test_percentage_of_correct_guesses
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
