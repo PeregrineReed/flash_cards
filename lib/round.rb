f15fe4067d6232f568a3bd03b2f5eefeea789096
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-11-28 15:36:08 -0700

    Added methods to Round class to meet RoundTest expectations

--- lib/round.rb
+++ lib/round.rb
@@ -12,7 +12,32 @@ class Round
 
   def take_turn(guess)
     turns << Turn.new(guess, current_card)
-    turns[0]
+    deck.cards = deck.cards.rotate(1)
+    @current_card = deck.cards.first
+    turns.last
+  end
+
+  def number_correct
+    turns.count {|turn| turn.correct?}
+  end
+
+  def number_correct_by_category(category)
+    turns.count do |turn|
+      turn.correct? && turn.card.category == category
+    end
+  end
+
+  def percent_correct
+    (number_correct.to_f / turns.length) * 100
+  end
+
+  def percent_correct_by_category(category)
+    turns_in_category = turns.count do |turn|
+      turn.card.category == category
+    end
+    percent = (number_correct_by_category(category).to_f / (turns_in_category)) * 100
+    percent = percent.round if percent == 0.0
+    percent
   end
 
 end
