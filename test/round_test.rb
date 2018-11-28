39c1d2e4a26b512fe8dec210d19fd43e82cfe2f2
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-11-27 21:17:20 -0700

    Added test method for current card changing after a turn

--- test/round_test.rb
+++ test/round_test.rb
@@ -43,6 +43,19 @@ class RoundTest < Minitest::Test
     assert_same round.turns[0], new_turn
   end
 
+  def test_that_taking_a_turn_changes_the_current_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    assert_same deck.cards[0], round.current_card
+    round.take_turn("Juneau")
+    assert_includes round.turns, card_1
+    assert_same deck.cards[0], round.current_card && card_2
+  end
+
   def test_percentage_of_correct_guesses_is_logged
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
