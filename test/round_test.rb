64738bf370f4f78069de30bd4a02f4e69f94767e
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-11-27 21:01:52 -0700

    Added method for percentage of correct guesses by category

--- test/round_test.rb
+++ test/round_test.rb
@@ -43,7 +43,7 @@ class RoundTest < Minitest::Test
     assert_same round.turns[0], new_turn
   end
 
-  def test_percentage_of_correct_guesses_are_tracked_in_each_round
+  def test_percentage_of_correct_guesses_is_logged
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -54,10 +54,24 @@ class RoundTest < Minitest::Test
     round.turns[0].correct?
     assert_equal 100.0, round.percent_correct
     assert_instance_of Float, round.percent_correct
+
+    round.take_turn("Venus")
+    assert_equal 50.0, round.percent_correct
   end
 
-  
+  def test_percentage_of_correct_guesses_for_category #add specific category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
 
-
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+    assert_equal 100.0, round.percent_correct_by_category(:Geography)
+    assert_instance_of Float, round.percent_correct_by_category(:Geography)
+    assert_equal 0,round.percent_correct_by_category(:STEM)
+    assert_instance_of Integer, round.percent_correct_by_category(:STEM)
+  end
 
 end
