9df4c0f8b87b7330089d4c1c7842d4e5ae7f88e3
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-11-29 13:49:29 -0700

    Moved start method to round from runner, fixed method to run

--- lib/round.rb
+++ lib/round.rb
@@ -37,4 +37,30 @@ class Round
     percent
   end
 
+
+  def start
+
+    puts "Welcome! You're playing with #{deck.cards.count} cards"
+    puts "-" * 49
+    # iterate for each question
+    questions = deck.cards.map do |card|
+      "This is card number #{deck.cards.index(card)} of #{deck.cards.count}."
+      "Question: #{card.question}"
+    end
+    questions.each do |question|
+      puts question
+      input = gets.chomp.downcase.capitalize
+      take_turn(input)
+      puts turns.last.feedback
+    end
+
+    puts "****** Game over! ******"
+    puts "You had #{number_correct} correct guesses out of #{deck.cards.count} for a total score of #{percent_correct.round(1)}%"
+    #iterate for each category
+    categories = deck.cards.uniq { |card| card.category }
+    category_scores = categories.map do |card|
+      puts "#{card.category.to_s} - #{percent_correct_by_category(card.category)}% correct"
+    end
+  end
+
 end
