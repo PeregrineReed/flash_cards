d69808a371f8be7007a193a991310ce958bfcaa1
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-12-01 13:47:44 -0700

    Shifted start method from Round class to Start module

--- lib/start.rb
+++ lib/start.rb
@@ -1,3 +1,31 @@
-class Start
+module Start
+
+  def start
+
+    puts ""
+    puts ""
+    puts "Welcome! You're playing with #{deck.cards.count} cards"
+    puts "-" * 49
+    # iterate for each question
+    questions = deck.cards.map do |card|
+      "This is card number #{deck.cards.index(card)} of #{deck.cards.count}."
+      "Question: #{card.question}"
+    end
+    questions.each do |question|
+      puts question
+      input = gets.chomp.downcase.capitalize
+      take_turn(input)
+      puts turns.last.feedback
+    end
+
+    puts "****** Game over! ******"
+    puts "You had #{number_correct} correct guesses out of #{deck.cards.count} for a total score of #{percent_correct.to_i}%"
+
+    categories.each do |card|
+      puts "#{card.category.to_s} - #{percent_correct_by_category(card.category).to_i}% correct"
+    end
+    puts ""
+    puts ""
+  end
 
 end
